4c55338899c177c39294a8d41ea845694fedcbae
Kevin Bernfeld <kcbernfeld@gmail.com>
2017-05-12 20:43:04 +0000

    this is fun

--- src/components/idol.tsx
+++ src/components/idol.tsx
@@ -8,20 +8,10 @@ export default class Idol extends React.Component<any, any> {
     render() {
         return (
             <div className="pure-u-1">
-                <div className="pure-u-1-4">
-                    <img src="http://placehold.it/150x150" />
-                </div>
-                <div className="pure-u-1-2">
-                    <div className="pure-u-1">
-                        <p>this is an idol</p>
-                    </div>
-                    <div className="pure-u-1">
-                        <p>kitten has some stats i guess</p>
-                    </div>
-                </div>
-                <div className="pure-u-1-4">
-                    <button className="pure-button button-primary">pick me</button>
-                </div>
+                <img src="http://placehold.it/250x350" /><br />
+	        <p>this is an idol</p>
+	        <p>kitten has some stats i guess</p>
+                <button className="pure-button button-primary">pick me</button>
             </div>
         )
     }
--- src/components/subunit.tsx
+++ src/components/subunit.tsx
@@ -7,10 +7,20 @@ export default class Subunit extends React.Component<any, any> {
     }
 
     render() {
+	let children = this.props.children.map((item, index) => {
+		return (
+		    <div className="pure-u-1-4">
+			{item}
+		    </div>
+		);
+	});
         return (
             <div>
                 <h2>aqours</h2>
-                {this.props.children}
+		{children}
+		<div className="pure-u-1-4">
+		    i am the code god. it's like google docs but shittier
+		</div>
             </div>
         )
     }
