73ffc6e9c3f320eca90e1ab94ae332cff263e5c4
Kevin Bernfeld <kcbernfeld@gmail.com>
2017-05-30 16:11:06 -0400

    adding vendor config for webpack

--- ohheck.help/webpack.config.js
+++ ohheck.help/webpack.config.js
@@ -1,33 +1,41 @@
 const webpack = require('webpack');
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 
-module.exports = {
-    stats: { modules: false },
-    entry: {
-        main: ['./src/index.tsx']
-    },
-    resolve: {
-        extensions: [ '.js', '.jsx', '.ts', '.tsx' ]
-    },
-    output: {
-        path: `${__dirname}/wwwroot/js`,
-        publicPath: '/js/',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.ts(x?)?$/, include: /src/, use: { loader: 'babel-loader', options: { cacheDirectory: true } } },
-            { test: /\.tsx?$/, include: /src/, use: 'awesome-typescript-loader?silent=true' },
-            {
-                test: /\.(scss|css)$/, 
-                use: [
-                    { loader: 'style-loader' },
-                    { loader: 'css-loader' },
-                    { loader: 'sass-loader' }
-                ]
-            }
+module.exports = env => {
+    return [{
+        stats: { modules: false },
+        entry: {
+            main: ['./src/index.tsx']
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx']
+        },
+        output: {
+            path: `${__dirname}/wwwroot/js`,
+            publicPath: '/js/',
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                { test: /\.ts(x?)?$/, include: /src/, use: { loader: 'babel-loader', options: { cacheDirectory: true } } },
+                { test: /\.tsx?$/, include: /src/, use: 'awesome-typescript-loader?silent=true' },
+                {
+                    test: /\.(scss|css)$/,
+                    use: [
+                        { loader: 'style-loader' },
+                        { loader: 'css-loader' },
+                        { loader: 'sass-loader' }
+                    ]
+                }
+            ]
+        },
+        devtool: 'inline-source-map',
+        plugins: [
+            new CheckerPlugin(),
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: require('./wwwroot/js/vendor-manifest.json')
+            })
         ]
-    },
-    devtool: 'inline-source-map',
-    plugins: [new CheckerPlugin()]
+    }];
 };
---
+++ ohheck.help/webpack.config.vendor.js
@@ -0,0 +1,37 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+
+module.exports = (env) => {
+    return [{
+        stats: { modules: false },
+        resolve: {
+            extensions: [ '.js' ]
+        },
+        module: {
+            rules: [
+                { test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: 'url-loader?limit=100000' }
+            ]
+        },
+        entry: {
+            vendor: [
+                'whatwg-fetch',
+                'react',
+                'react-dom',
+                'react-router-dom',
+                'react-markdown'
+            ],
+        },
+        output: {
+            path: path.join(__dirname, 'wwwroot', 'js'),
+            publicPath: '/js/',
+            filename: '[name].js',
+            library: '[name]_[hash]',
+        },
+        plugins: [
+            new webpack.DllPlugin({
+                path: path.join(__dirname, 'wwwroot', 'js', '[name]-manifest.json'),
+                name: '[name]_[hash]'
+            })
+        ]
+    }];
+};
