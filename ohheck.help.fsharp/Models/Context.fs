73f3176c1e0295a7f0c4fd9b78775bec7b43a08f
Kevin Bernfeld <kcbernfeld@gmail.com>
2017-11-23 04:27:11 -0500

    i want to write f# so badly

--- ohheck.help.fsharp/Models/Context.fs
+++ ohheck.help.fsharp/Models/Context.fs
@@ -3,17 +3,17 @@ namespace ohheck.help.fsharp
 open FSharp.Data.Sql
 
 module HeckingContext =
-    let [<Literal>] resPath = "bin/Debug/netcoreapp2.0"
+    let [<Literal>] resPath = "./bin/Debug/netcoreapp2.0";;
 
     type Context = SqlDataProvider<
                         Common.DatabaseProviderTypes.POSTGRESQL,
                         "Host=localhost;Database=ohheck;Username=ohheck;Password=password",
-                        ResolutionPath = resPath,
-                        UseOptionTypes = true>
+                        ResolutionPath = "/Users/kevinbernfeld/projects/ohheck.help/ohheck.help.fsharp/bin/Debug/netcoreapp2.0/",
+                        UseOptionTypes = true>;;
     
-    let ctx = Context.GetDataContext()
+    let ctx = Context.GetDataContext();;
 
-    printfn "%A" ctx
+    printfn "%A" ctx;;
     (*let surveys = 
         query {
             for survey in ctx.public.Surveys do
